3fba377297c3aae70f966feb7d7f7f5c2301b616
carlos calderon <cdcalderon@gmail.com>
2020-03-05 00:57:08 -0600

    Initial Commit create skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { ChartsComponent } from './pages/charts/charts.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    ChartsComponent
   ],
   imports: [
     BrowserModule
